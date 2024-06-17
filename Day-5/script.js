592186ad86a6208c0c5061bc4b1c067bdaff4ea3
Ashish kumar <72202312+branwalashish@users.noreply.github.com>
2024-06-17 18:14:49 +0530

    Add files via upload

---
+++ Day-5/script.js
@@ -0,0 +1,8 @@
+function dark(){
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white"
+}
+function light(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black"
+}
